17fdb10a66a069a23e6bf9d662a2cbec7550c1ee
2Ah <bashneved@gmail.com>
2016-08-25 17:17:52 +0700

    1

---
+++ UARTDHT-Client/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace UARTDHT_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labInfoDHT = new System.Windows.Forms.Label();
+            this.timerServerRequest = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataTable1 = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labInfoConnect = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.mskTCP = new System.Windows.Forms.MaskedTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labInfoDHT);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 61);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные датчика:";
+            // 
+            // labInfoDHT
+            // 
+            this.labInfoDHT.AutoSize = true;
+            this.labInfoDHT.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labInfoDHT.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labInfoDHT.Location = new System.Drawing.Point(8, 16);
+            this.labInfoDHT.Name = "labInfoDHT";
+            this.labInfoDHT.Size = new System.Drawing.Size(89, 17);
+            this.labInfoDHT.TabIndex = 25;
+            this.labInfoDHT.Text = "Нет данных.";
+            // 
+            // timerServerRequest
+            // 
+            this.timerServerRequest.Interval = 9000;
+            this.timerServerRequest.Tick += new System.EventHandler(this.timerServerRequest_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "UARTDHT-Client";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            this.dataSet1.Tables.AddRange(new System.Data.DataTable[] {
+            this.dataTable1});
+            // 
+            // dataTable1
+            // 
+            this.dataTable1.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2});
+            this.dataTable1.TableName = "Table1";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.ColumnName = "Column1";
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "Column2";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.labInfoConnect);
+            this.panel2.Controls.Add(this.btnDisconnect);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.numPort);
+            this.panel2.Controls.Add(this.mskTCP);
+            this.panel2.Controls.Add(this.btnConnect);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(384, 110);
+            this.panel2.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Статус подключения: ";
+            // 
+            // labInfoConnect
+            // 
+            this.labInfoConnect.AutoSize = true;
+            this.labInfoConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labInfoConnect.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labInfoConnect.Location = new System.Drawing.Point(3, 62);
+            this.labInfoConnect.Name = "labInfoConnect";
+            this.labInfoConnect.Size = new System.Drawing.Size(186, 17);
+            this.labInfoConnect.TabIndex = 21;
+            this.labInfoConnect.Text = "Подключение отсутствует.";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(288, 15);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(90, 23);
+            this.btnDisconnect.TabIndex = 20;
+            this.btnDisconnect.Text = "Отключиться";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 2);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Порт сервера:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "TCP адрес сервера:";
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(112, 18);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Minimum = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(77, 20);
+            this.numPort.TabIndex = 16;
+            this.numPort.Value = new decimal(new int[] {
+            1024,
+            0,
+            0,
+            0});
+            // 
+            // mskTCP
+            // 
+            this.mskTCP.Location = new System.Drawing.Point(3, 18);
+            this.mskTCP.Mask = "000\\.000\\.000\\.000";
+            this.mskTCP.Name = "mskTCP";
+            this.mskTCP.PromptChar = ' ';
+            this.mskTCP.Size = new System.Drawing.Size(103, 20);
+            this.mskTCP.TabIndex = 15;
+            this.mskTCP.Text = "1270000001";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(195, 15);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(90, 23);
+            this.btnConnect.TabIndex = 14;
+            this.btnConnect.Text = "Подключиться";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 171);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UARTDHT - Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTable1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        internal System.Windows.Forms.Label labInfoDHT;
+        private System.Windows.Forms.Timer timerServerRequest;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Data.DataSet dataSet1;
+        private System.Data.DataTable dataTable1;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label labInfoConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.MaskedTextBox mskTCP;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ UARTDHT-Client/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UARTDHT_Client
+{
+  public partial class Form1 : Form
+  {
+    bool isAllTimerEnd = true; // Для блокирования процедуры таймера, если предыдущая еще не завершена. 
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnConnect_Click(object sender, EventArgs e)
+    {
+      mskTCP.Enabled = false;
+      numPort.Enabled = false;
+      btnConnect.Enabled = false;
+      timerServerRequest.Enabled = true;
+      notifyIcon1.Icon = Properties.Resources.HT_16;
+    }
+
+    private void btnDisconnect_Click(object sender, EventArgs e)
+    {
+      timerServerRequest.Enabled = false;
+      mskTCP.Enabled = true;
+      numPort.Enabled = true;
+      btnConnect.Enabled = true;
+      labInfoConnect.Text = "Соединение завершено.";
+      labInfoDHT.Text = "";
+      notifyIcon1.Icon = Properties.Resources.HT_16_bw;
+    }
+
+    private void timerServerRequest_Tick(object sender, EventArgs e)
+    {
+      if (!isAllTimerEnd) return;
+      isAllTimerEnd = false;
+      int serverPort = (int)numPort.Value;
+      string message = DateTime.Now.ToLongTimeString() + ": ",
+        serverAddress = mskTCP.Text.Replace(" ", "");
+      labInfoConnect.Text = "";
+      labInfoDHT.Text = "";
+
+      TcpClient client = new TcpClient();
+      NetworkStream stream = null;
+      try
+      {
+        labInfoConnect.Text = "Подключение к серверу на " + serverAddress + ":" + serverPort;
+        client.Connect(IPAddress.Parse(serverAddress), serverPort);
+        labInfoConnect.Text += "\nПодключение установлено.";
+        stream = client.GetStream();
+        stream.ReadTimeout = 999; // Через 1 сек. ожидания объектом NetworkStream ответного сообщения от сервера, генерируем Exception.
+        byte[] data = Encoding.Unicode.GetBytes("DHTClient"); // преобразуем сообщение в массив байтов 
+        stream.Write(data, 0, data.Length); // отправка сообщения  
+
+        data = new byte[256]; // Буфер для получаемых данных (символ Unicode - 2 байта).
+        int bytes = stream.Read(data, 0, data.Length); // Читаем первые 256 байтов сообщения.
+        message += Encoding.Unicode.GetString(data, 0, bytes);
+
+        labInfoDHT.Text = message;
+      }
+      catch (SocketException ex)
+      {
+        if (ex.ErrorCode == 10060)
+        {
+          message += "SocketException:\nОт хоста " + serverAddress + ":" + serverPort + " нет ответа.";
+          btnDisconnect.PerformClick();
+        }
+        else if (ex.ErrorCode == 10061) message += "SocketException:\nПо адресу " + serverAddress + ":" + serverPort + " отвергнуто подключение.";
+        else message += "SocketException: " + ex;
+        labInfoConnect.Text = message;
+      }
+      catch (Exception ex)
+      {
+        message += "Exception: " + ex;
+        labInfoConnect.Text = message;
+      }
+      finally
+      {
+        if (stream != null) stream.Close();
+        if (client != null) client.Close();
+        TheBalloonatic(message);
+        isAllTimerEnd = true;
+      }
+    }
+
+    private void TheBalloonatic(string message)
+    {
+      bool isPanic = false;
+      if (message.IndexOf("Exception:") >= 0) isPanic = true; // + "SocketException:"
+      else if (message.IndexOf("Датчик на сервере не обнаружен") >= 0) isPanic = true;
+      else if (message.IndexOf("Ошибка преобразования") >= 0) isPanic = true;
+      else if (message.IndexOf("Критическая ") >= 0) isPanic = true;
+      //MessageBox.Show(message);
+      if (isPanic)
+      {
+        notifyIcon1.BalloonTipTitle = "UARTDHT-Client";
+        notifyIcon1.BalloonTipText = message;
+        notifyIcon1.ShowBalloonTip(2000);
+      }
+    }
+
+    private void Form1_Resize(object sender, EventArgs e)
+    {
+      if (WindowState == FormWindowState.Minimized) Hide();
+    }
+
+    private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+    {
+      Show();
+      WindowState = FormWindowState.Normal;
+    }
+
+    private void Form1_Shown(object sender, EventArgs e)
+    { //Загрузка конфигурации из директории программы. 
+      bool isConnect = false;
+      System.Data.DataSet ds = new System.Data.DataSet();
+      try
+      {
+        ds.ReadXmlSchema(Application.StartupPath + "\\UARTDHT-Client.cfg");
+        mskTCP.Text = (string)ds.ExtendedProperties["tcp"];
+        numPort.Value = decimal.Parse((string)ds.ExtendedProperties["tcpPort"]);
+        isConnect = Convert.ToBoolean(ds.ExtendedProperties["isConnect"]);
+      }
+      catch { }
+      if (isConnect) btnConnect.PerformClick();
+    }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    { //Сохранение конфигурации в директории программы.
+      System.Data.DataSet ds = new System.Data.DataSet();
+      ds.ExtendedProperties["tcp"] = mskTCP.Text;
+      ds.ExtendedProperties["tcpPort"] = numPort.Value;
+      ds.ExtendedProperties["isConnect"] = timerServerRequest.Enabled;
+      ds.WriteXmlSchema(Application.StartupPath + "\\UARTDHT-Client.cfg");
+    }
+  }
+}
